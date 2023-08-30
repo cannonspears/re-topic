ec925899735769aad82772d714b5229ffce89a53
Cannon Spears <cannonspears@gmail.com>
2023-08-30 16:07:06 -0500

    Renamed classes

--- src/topics/RealEstate.js
+++ src/topics/RealEstate.js
@@ -3,7 +3,7 @@ import React from "react";
 function RealEstate() {
   return (
     <div className="topic">
-      <div className="real-estate" title="Real Estate">
+      <div className="category" title="Real Estate">
         Real Estate
       </div>
       <blockquote>Top business tips</blockquote>
