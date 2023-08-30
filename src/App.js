6ee677e3eabec39ddb1631ae02da9812dfa3076a
Cannon Spears <cannonspears@gmail.com>
2023-08-30 16:21:23 -0500

    State variables and api fetch request

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React, { useState, useEffect } from "react";
 import RealEstate from "./topics/RealEstate";
 
 function App() {
+  const [topic, setTopic] = useState({
+    topic: null,
+  });
+
+  const fetchTopic = async () => {
+    return await fetch("https://topicsgenerator.onrender.com/real-estate").then((response) => {
+      response.json();
+    });
+  };
+
+  useEffect(async () => {
+    setTopic(await fetchTopic());
+  }, []);
+
   return (
     <div className="App">
-      <RealEstate />
+      <RealEstate topic={topic} />
       <button>Generate New Topic</button>
     </div>
   );
