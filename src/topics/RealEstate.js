639dc3709a801fdf26d5c518d1a2d7f13f771a46
Cannon Spears <cannonspears@gmail.com>
2023-08-30 15:50:35 -0500

    Real Estate Topics component

---
+++ src/topics/RealEstate.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function RealEstate() {
+  return (
+    <div className="topic">
+      <div className="real-estate" title="Real Estate">
+        Real Estate
+      </div>
+      <blockquote>Top business tips</blockquote>
+    </div>
+  );
+}
+
+export default RealEstate;
