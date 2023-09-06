f31c66ef6212e47abf0c4941400aecfc1e6ae60e
Cannon Spears <cannonspears@gmail.com>
2023-09-06 16:48:28 -0500

    Removed debug lines

--- src/topics/RealEstate.js
+++ src/topics/RealEstate.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 function RealEstate({ topic }) {
-  console.log("TOPIC", topic);
   return (
     <div className="topic">
       <div className="category" title="Real Estate">
