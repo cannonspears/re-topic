34aeb04d39a4d32bcf2b636b30d05540b764df98
Cannon Spears <cannonspears@gmail.com>
2023-09-07 12:56:54 -0500

    Styling button and TODO notes

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -1,15 +1,29 @@
 import React from "react";
+import { IconContext } from "react-icons";
+import { HiArrowRight } from "react-icons/hi";
 
-function Topic({ category, topic }) {
+function Topic({ category, topic, script }) {
+  // TODO: Functional Script button
+  // Need: Grey out and make not clickable if no script available in fetched data
+  // Binary state variable to show/hide script if available
+  // Change button and title text to "Show" or "Hide"
+
   return (
     <div className="topic">
-      <div className="category" title={category}>
-        {category}
+      <div className="container">
+        <button className="category" title={category}>
+          {category}
+          <IconContext.Provider value={{ style: { verticalAlign: "middle", marginLeft: 10 } }}>
+            <HiArrowRight />
+          </IconContext.Provider>
+        </button>
       </div>
       <blockquote>{topic}</blockquote>
-      <button className="next" title="Next Category">
-        Next Category
-      </button>
+      <div className="container">
+        <button className="script" title="Show Script">
+          Show Script
+        </button>
+      </div>
     </div>
   );
 }
