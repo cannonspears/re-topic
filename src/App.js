cc3d396cb29a6d3c4c2c78314ce36aa49899486b
Cannon Spears <cannonspears@gmail.com>
2023-09-12 12:14:01 -0500

    Refactored for maintainability

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Topic from "./components/Topic";
 import validCategories from "./utils/validCategories";
 
-function App() {
-  const [topic, setTopic] = useState("Loading...");
-  const [categoryInd, setCategoryInd] = useState(0);
-  const [topicList, setTopicList] = useState([]);
+const API_URL = "https://topicsgeneratorapi.onrender.com/";
 
-  const category = validCategories[categoryInd];
+const DEFAULT_LOADING_TEXT = "Loading...";
+
+function App() {
+  const [state, setState] = useState({
+    topic: DEFAULT_LOADING_TEXT,
+    categoryInd: 0,
+    topicList: [],
+  });
+
+  const { topic, categoryInd, topicList } = state;
 
   const fetchCategory = async () => {
     try {
-      const response = await fetch(`https://topicsgeneratorapi.onrender.com/${category.endpoint}`);
+      const response = await fetch(`${API_URL}${validCategories[categoryInd].endpoint}`);
 
       if (!response.ok) {
         throw new Error(`Request failed with status: ${response.status}`);
       }
+
       const { data } = await response.json();
       return data;
     } catch (error) {
@@ -24,31 +31,39 @@ function App() {
     }
   };
 
-  const categoryHandler = () => {
-    setCategoryInd((currentInd) =>
-      currentInd !== validCategories.length - 1 ? currentInd + 1 : 0
-    );
-  };
+  const categoryHandler = useCallback(() => {
+    setState((prevState) => ({
+      ...prevState,
+      categoryInd:
+        prevState.categoryInd !== validCategories.length - 1 ? prevState.categoryInd + 1 : 0,
+    }));
+  }, []);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchCategory();
       if (data && data.length > 0) {
-        setTopicList(data);
-        const randomIndex = Math.floor(Math.random() * data.length);
-        setTopic(data[randomIndex].topic);
+        setState((prevState) => ({
+          ...prevState,
+          topicList: data,
+          topic: data[Math.floor(Math.random() * data.length)].topic,
+        }));
       }
     };
 
     fetchData();
   }, [categoryInd]);
 
-  const generate = async () => {
+  const generate = () => {
     if (topicList && topicList.length > 0) {
-      const randomIndex = Math.floor(Math.random() * topicList.length);
-      setTopic(topicList[randomIndex].topic);
+      setState((prevState) => ({
+        ...prevState,
+        topic: topicList[Math.floor(Math.random() * topicList.length)].topic,
+      }));
     }
   };
+
+  const category = validCategories[categoryInd];
 
   return (
     <div className="App">
