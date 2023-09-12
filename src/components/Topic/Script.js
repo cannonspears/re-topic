58bb7c3b9c8df34ba1d2748eee155cc43a18bf36
Cannon Spears <cannonspears@gmail.com>
2023-09-12 14:32:26 -0500

    Display new lines based off json response

--- src/components/Topic/Script.js
+++ src/components/Topic/Script.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 function Script({ script }) {
+  const lines = script.body.split("\n").map((newLine, index) => <p key={index}>{newLine}</p>);
   return (
     <div className="script">
       <p>{script.hook}</p>
-      <p>{script.body}</p>
+      {lines}
       <p>{script.cta}</p>
     </div>
   );
