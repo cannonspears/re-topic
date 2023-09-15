e1dbebbd4b0dab1026d6abc5e4859b32e2f3e55f
Cannon Spears <cannonspears@gmail.com>
2023-09-14 21:54:01 -0500

    Renamed button class

--- src/components/Buttons/NewTopicButton.js
+++ src/components/Buttons/NewTopicButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function NewTopicButton({ onClick }) {
   return (
-    <button className="generateButton" onClick={onClick}>
+    <button className="NewTopicButton" onClick={onClick}>
       Generate New Topic
     </button>
   );
