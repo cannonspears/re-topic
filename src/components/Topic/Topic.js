64b5cb9164e5eb1fc44c9c8d4f2e2a3919920d3d
Cannon Spears <cannonspears@gmail.com>
2023-09-20 16:42:44 -0500

    Removed TODO

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import Script from "./Script";
 
 function Topic({ topic, script }) {
-  // TODO: Grey out button and make not clickable if no script available in fetched data
-
   const [showScript, setShowScript] = useState(false);
 
   const showScriptHandler = () => {
