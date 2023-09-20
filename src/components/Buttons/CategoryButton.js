2a8794d1e744fa7af8b6dffff4df53142ed3bb3b
Cannon Spears <cannonspears@gmail.com>
2023-09-20 12:44:46 -0500

    Added next arrow icon

--- src/components/Buttons/CategoryButton.js
+++ src/components/Buttons/CategoryButton.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { IconContext } from "react-icons";
+import { HiArrowRight } from "react-icons/hi";
 
 function CategoryButton({ onClick, title }) {
   return (
     <button onClick={onClick} className="categoryButton" title={title}>
       {title}
+      <IconContext.Provider value={{ style: { verticalAlign: "middle", marginLeft: 10 } }}>
+        <HiArrowRight />
+      </IconContext.Provider>
     </button>
   );
 }
