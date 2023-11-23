3e8c4234c7ecbaf900942836ed780177d2ecce8e
Cannon Spears <cannonspears@gmail.com>
2023-11-23 13:12:26 -0600

    Renamed loading text

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [topics, setTopics] = useState([]);
   const [currentTopic, setCurrentTopic] = useState({
     id: "",
-    topic: "Loading Topic",
+    topic: "Loading Topic...",
     script: "",
   });
 
