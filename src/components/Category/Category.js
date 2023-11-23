dca8dbd7f31af6f1bfd59bebffb68b9f2cccfde1
Cannon Spears <cannonspears@gmail.com>
2023-11-23 13:08:49 -0600

    Styled Category Selection

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,17 +1,19 @@
 import React from "react";
-import Button from "@mui/material/Button";
+import { Button, Typography, Stack } from "@mui/material";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
 
 function Category({ previousClick, nextClick, currentCategory }) {
   return (
-    <div>
+    <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" pt={2}>
       <Button variant="outlined" onClick={previousClick} title="Previous Category">
-        Previous
+        <KeyboardDoubleArrowLeftIcon />
       </Button>
-      <div>{currentCategory.name}</div>
+      <Typography>{currentCategory.name}</Typography>
       <Button variant="outlined" onClick={nextClick} title="Next Category">
-        Next
+        <KeyboardDoubleArrowRightIcon />
       </Button>
-    </div>
+    </Stack>
   );
 }
 
