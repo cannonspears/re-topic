1bfd5b0153e349bac6294b93ddbcb2dac9e67adb
Cannon Spears <cannonspears@gmail.com>
2023-11-24 20:06:45 -0600

    Styling details

--- src/components/Topic/Details.js
+++ src/components/Topic/Details.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Typography } from "@mui/material";
 
 function Details({ script, id }) {
   const lines = script.body.split("\n").map((newLine, index) => <p key={index}>{newLine}</p>);
 
   return (
-    <div>
+    <Typography textAlign="left" px={12}>
       <p>{script.hook}</p>
       {lines}
       <p>{script.cta}</p>
       <div>Topic ID {id}</div>
-    </div>
+    </Typography>
   );
 }
 
