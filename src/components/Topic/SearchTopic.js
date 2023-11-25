b4e97e3091cd0670bc4682b7317cf187d4e24c33
Cannon Spears <cannonspears@gmail.com>
2023-11-24 20:45:30 -0600

    Removed Search Button

--- src/components/Topic/SearchTopic.js
+++ src/components/Topic/SearchTopic.js
@@ -1,32 +1,24 @@
 import React, { useState } from "react";
-import Button from "@mui/material/Button";
-import { TextField, Stack } from "@mui/material";
+import { TextField } from "@mui/material";
 
 function SearchTopic({ onSearch }) {
   const [searchId, setSearchId] = useState("");
-  const [searchResult, setSearchResult] = useState("");
 
   const handleSearch = async () => {
-    const result = await onSearch(searchId);
-    setSearchResult(result);
+    await onSearch(searchId);
   };
 
   return (
-    <div>
-      <Stack direction="row" spacing={2} alignItems="center" justifyContent="center">
-        <TextField
-          id="outlined-basic"
-          label="Search by Topic ID"
-          variant="outlined"
-          type="text"
-          size="small"
-          value={searchId}
-          onChange={(e) => setSearchId(e.target.value)}
-        />
-        <Button onClick={handleSearch}>Search</Button>
-      </Stack>
-      {/* {searchResult && <p>{searchResult}</p>} */}
-    </div>
+    <TextField
+      id="outlined-basic"
+      label="Search by Topic ID"
+      variant="outlined"
+      type="text"
+      size="small"
+      value={searchId}
+      onChange={(e) => setSearchId(e.target.value)}
+      onKeyDown={(e) => (e.keyCode === 13 ? handleSearch() : null)}
+    />
   );
 }
 
